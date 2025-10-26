575cda5fc4313ba7be3518d3f8e9408a3112af8a
yuio7279 <yuio7279@gmail.com>
2025-10-27 06:59:56 +0900

    init

---
+++ src/main/java/com/practice/dddExp/DddExpApplication.java
@@ -0,0 +1,13 @@
+package com.practice.dddExp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddExpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddExpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/dddExp/DddExpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.dddExp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddExpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
