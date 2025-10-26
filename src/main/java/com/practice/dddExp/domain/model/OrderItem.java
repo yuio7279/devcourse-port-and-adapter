b9ab892b4eb77187b1adf27662baaf26937eaaf9
yuio7279 <yuio7279@gmail.com>
2025-10-27 08:17:11 +0900

    [feat] OrderItem 스펙을 위한 검증

--- src/main/java/com/practice/dddExp/domain/model/OrderItem.java
+++ src/main/java/com/practice/dddExp/domain/model/OrderItem.java
@@ -11,10 +11,20 @@ public class OrderItem {
 
 
     public OrderItem(OrderItemSpec spec) {
+        validation(spec);
         this.code = UUID.randomUUID().toString();
         this.spec = spec;
     }
 
+    private void validation(OrderItemSpec spec){
+        Long price = spec.price();
+        Long quantity = spec.quantity();
+        String name = spec.name();
+
+        if(price <= 0) throw new IllegalArgumentException("가격은 반드시 0원 이상이어야 합니다.");
+        if(quantity <= 0) throw new IllegalArgumentException("수량은 반드시 1개 이상이어야 합니다.");
+        if(name == null || name.isEmpty()) throw new IllegalArgumentException("이름을 반드시 정해야 합니다.");
+    }
 
     public String getCode() {
         return code;
